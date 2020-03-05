4c9d16ced31bc433ea8408903484d50abebef184
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2020-03-05 14:32:32 -0300

    atividade 1 tema 3 AV!

---
+++ ATIVIDADE1/ATIVIDADE1.C
@@ -0,0 +1,26 @@
+// Uma revendedora de carros usados paga a seus funcionários vendedores 
+// um salário fixo por mês, mais uma comissão também fixa para cada 
+// carro vendido e mais 5% do valor das vendas por ele efetuadas. 
+// Escrever um programa que leia o número de carros por ele vendidos, 
+// o valor total de suas vendas, o salário fixo e o valor que ele recebe 
+// por carro vendido. 
+// Calcule e escreva o salário final do vendedor.
+#include <stdio.h>
+int main() {
+    int nCarVend;
+    float totalVendas, totalComFixa, comFixa, porcVenda, salFixo, salFinal;
+
+    printf("Numero de carros vendidos: "); scanf("%d", &nCarVend);
+    printf("Valor total de vendas: "); scanf("%f", &totalVendas);
+    printf("Salario fixo: "); scanf("%f", &salFixo);
+    printf("Valor fixo por carro vendido: "); scanf("%f", &comFixa);
+
+    totalComFixa = nCarVend*comFixa;
+    porcVenda = totalVendas*0.05;
+    salFinal = totalComFixa + porcVenda + salFixo + totalVendas;
+
+    printf("====================\n");
+    printf("Salario final do vendedor: R$ %.2f", salFinal);
+    return 0;
+
+}
