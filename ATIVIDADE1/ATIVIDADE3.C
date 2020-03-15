54b06b047b17e1fba3839e3500dbe1f5dce65528
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2020-03-15 07:33:44 -0300

    declaracao de variaveis e leia de dados

--- ATIVIDADE1/ATIVIDADE3.C
+++ ATIVIDADE1/ATIVIDADE3.C
@@ -2,5 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-    
+    int nBrigadeiro, nBrigEstoque, estoque, dia;
+    float porcentagem, precoBrig;
+
+    printf("Digite a quantidade de brigadeiros comprados: "); scanf("%d", nBrigadeiro);
+    printf("Digite a quantidade em estoque: ");scanf("%d", nBrigEstoque);
+    printf("Número do dia da semana (Ex: 1- Domingo): "), scanf("%d", dia);
 }
