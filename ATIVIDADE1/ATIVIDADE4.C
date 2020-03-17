4d3278b207da25c809a85467f5a3d427c97da003
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2020-03-17 12:48:58 -0300

    CRIACAO DO ARQUIVO

---
+++ ATIVIDADE1/ATIVIDADE4.C
@@ -0,0 +1,2 @@
+//A Brasileiras.com está concedendo um desconto de 5% para as compras pagas com boleto. Faça um programa para ler o valor da compra e pergunte se será paga com boleta. Caso positivo, conceda o desconto 
+// e informe e valor a pagar.
