773a27c6a34f3aacb9ea1e897deba72c031f4c89
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2020-04-10 09:04:31 -0300

    exercicio 4 tema 6 for

--- TEMA6/tema6_ava_exerc4.c
+++ TEMA6/tema6_ava_exerc4.c
@@ -1,1 +1,16 @@
 // Faça um programa que imprima os múltiplos positivos de 7, inferiores a 100.
+#include <stdio.h>
+
+int main() {
+    int i, cont=100;
+    
+    printf("============IMPRESSAO=============\n");
+    printf("Os multiplos de 7 ate 100:\n");
+    for (i = 0; i < cont; i=i+7)
+    {
+        if (i%7==0)
+        {
+            printf("%d\n", i);
+        }
+    }
+}
