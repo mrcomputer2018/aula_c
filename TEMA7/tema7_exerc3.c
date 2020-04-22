9bf7d7b22a2a34a53dbe9a5d356091520aa85dc9
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2020-04-22 16:35:12 -0300

    acerto do programa e da logica

--- TEMA7/tema7_exerc3.c
+++ TEMA7/tema7_exerc3.c
@@ -7,8 +7,9 @@
 
 int main(){
     // declaracao de variaveis e atribuicao de valor a constante
-    float salMin, sal, mediaSal, mediaSalFinal, salMin3, func=0.0, porcSal3 = 0.0, contSal3=0.0;
-    float quantSalMin=0.0 ;
+    float salMin, sal, mediaSal, mediaSalFinal, salMin3, porcSal3 = 1.0;
+    float mediaSal2, quantSalMin=1.0 ;
+    int func, contSal3;
     // laco de repeticao e condicao
     while (sal>0)
     {
@@ -16,32 +17,29 @@ int main(){
         printf("Digite o salario minimo vingente (0 p/sair): R$\n"); scanf("%f", &salMin);
         printf("Digite o salario do funcionario (0 p/sair): R$\n"); scanf("%f", &sal);
         salMin3 = sal*3;
+        func=1;
+        contSal3=1;
         if (sal <= salMin)
         {
-            mediaSal = mediaSal + sal;
-            func = func + 1;
             quantSalMin++;
-            contSal3=0.0;
         }
-        else if (sal>=salMin3)
+        else if (sal >= salMin3)
         {
-            mediaSal = mediaSal + sal;
             contSal3++;
-                printf("%.2f contsal3 dentro\n", contSal3);
-            func = func + 1;
+            printf("%.2f contsal3 dentro\n", contSal3);
+            
         }
-           
-        
+        mediaSal = mediaSal + sal;
+        func = func + 1;
     }
-    printf("%.2f func\n", func);
+    printf("%d func\n", func);
     printf("%.2f contsal3\n", contSal3);
     printf("%.2f mediasal\n", mediaSal);
     quantSalMin= quantSalMin-1;
-    func = func-1;
-    porcSal3 = func/contSal3;
+    porcSal3 = (contSal3*100)/func;
     mediaSalFinal = mediaSal/func;
     printf("================IMPRESSAO================\n");
     printf("Quantd de funcs(salario Minimo) : %.2f\n", quantSalMin);
-    printf("Porcent de funcs(salario triplo): %.2f\n", porcSal3);
+    printf("Porcent de funcs(salario triplo): %.2f %%\n", porcSal3);
     printf("Media salarial de               : R$ %.2f\n", mediaSalFinal);
 }
