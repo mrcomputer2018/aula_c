ecc485456339664aeccae2ef11d498fd64c8bb8e
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2020-04-22 16:49:17 -0300

    enunciado do roblema

---
+++ TEMA7/tema7_exerc4.c
@@ -0,0 +1,19 @@
+// Foi realizada uma pesquisa de algumas características físicas da população de 
+// uma certa região, a qual coletaram os seguintes dados referentes a cada 
+// habitante para serem analisados:
+// - sexo (masculino e feminino)
+// - cor dos olhos (azuis, verdes ou castanhos)
+// - cor dos cabelos (louros, castanhos, pretos)
+// - idade
+// Faça um programa que determine e escreva:
+// a) a maior idade dos habitantes;
+// b) a quantidade de indivíduos do sexo feminino cuja idade está entre 18
+// e 35 anos, inclusive;
+// c) a quantidade de indivíduos que tenham olhos verdes e cabelos louros;
+// O final do conjunto de habitantes é reconhecido pelo valor -1 informado 
+// como idade.
+#include <stdio.h>
+
+int main () {
+
+}
