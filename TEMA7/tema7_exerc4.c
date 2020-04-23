796a0011e9c97f4a2dc8add4bc45947b13f85d32
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2020-04-23 11:25:38 -0300

    criando menus

--- TEMA7/tema7_exerc4.c
+++ TEMA7/tema7_exerc4.c
@@ -17,4 +17,16 @@
 int main () {
     int idade;
     char corOlho, corCabelo, sexo;
+
+    printf("===============MENU===============\n");
+    printf("Escolha uma das opcoes abaixo:\n");
+    printf("SEXO\n");
+    printf("(M) - Masculino\n");
+    printf("(F) - Feminino\n");
+    scanf("%c", &sexo);
+    printf("COR DOS OLHOS:\n");
+    printf("(A) - Azul\n");
+    printf("(V) - Verde\n");
+    printf("(C) - Castanho\n");
+    scanf("%c", &corOlho);
 }
