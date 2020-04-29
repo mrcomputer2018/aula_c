cbfb244cfd20643f9085173a809f0e039a62e9dd
Marcelo Ribeiro <mrcomputerinfo@gmail.com>
2020-04-29 16:31:04 -0300

    criacao do arquivo

---
+++ AV2/atividade1_av2.c
@@ -0,0 +1,15 @@
+// Considere que, para cada um dos hotéis fazenda da região, se tenha registrado o nome do hotel, a sua 
+// distância do centro da cidade (DA), o número médio de visitantes no último feriado (V) e o tipo de 
+// acesso ao hotel (AC sendo 0 – acesso não asfaltado; 1 – acesso asfaltado).
+// Construa um programa que leia esses dados e ao final informe:
+// a) O número de hotéis que distam mais de 15km do centro;
+// b) A quantidade média de visitantes no último feriado, nos hotéis com acesso não asfaltado;
+// c) O nome e a distância do centro em Km, de todos os hotéis de acesso asfaltado que tiveram menos de 1.000 visitantes.
+// Exemplo de entrada :
+// Hotel A, DA=10, V=100, AC=0
+// Hotel B, DA=20, V=50, AC=1.
+#include <stdio.h>
+
+int main() {
+    
+}
