184c6c4b86b4cc0fa1c60aeb1a6e9f97ad00c075
Marcelo Ribeiro <mrcomputerinfo@gmail.com>
2020-05-05 09:17:01 -0300

    criacao do arquivo base

---
+++ TEMA8/tema8_exerc5.c
@@ -0,0 +1,9 @@
+// Faça um programa que leia informações de alunos (Matricula, Nota1, Nota2 , Nota3) 
+// com o fim das informações indicado por Matricula = 9999 . Para cada aluno deve ser 
+// calculada a média final de acordo com a seguinte fórmula:
+// Média final = [(2 * Nota1) +(3* Nota2) +(4* Nota 3)] / 9
+// Se a média final for igual ou superior a 5, o algoritmo deve mostrar Matrícula, 
+// Média Final e a mensagem "APROVADO" ; se a média final for inferior a 5, o
+// algoritmo deve mostrar Matricula, Média Final e a mensagem "REPROVADO".
+// Ao final devem ser mostrados o total de aprovados, o total de alunos da turma 
+// e o total de reprovados
