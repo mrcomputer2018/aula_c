8c7400965ea747c191b401fbbc67943d38ce282e
Marcelo Ribeiro <mrcomputerinfo@gmail.com>
2020-05-05 17:15:58 -0300

    acertos 2

--- CODIGO/ADVINHACAO.C
+++ CODIGO/ADVINHACAO.C
@@ -9,9 +9,8 @@ int main() {
     printf("************************************\n");
     // declarando variaveis
     int numeroSecreto = 42, chute, ganhou = 0, tentativas = 1; 
-    double pontos = 1000;
     // criando o laco de repeticao
-    while (!=ganhou)
+    while (!ganhou)
     {
         // leitura de dados
         printf("Qual e o seu %do.chute?\n", tentativas); scanf("%d", &chute);
@@ -36,22 +35,22 @@ int main() {
             // impressao do numero se acertou de primeira
             if (acertou)
             {
-                printf("Parabens! Voce acertopu!\n");
+                printf("***Parabens! Voce acertou!***\n");
                 printf("Jogue de novo, voce e um bom jogador!\n");
                 break;
             }
             else if (maior)
             {
-                printf("Seu chute foi maior do que o numero screto!\n");
+                printf("Seu chute foi maior do que o numero secreto!\n");
             }
             // else condicao menor
             else
             {
-                printf("Seu chute foi menor do que o numero screto!\n");
+                printf("Seu chute foi menor do que o numero secreto!\n");
             }
             // contador
             tentativas++;
-            printf("Voce errou!\n");
+            printf("** Voce errou! **\n");
             printf("Mas nao desanime, tente de novo!\n");
         }
     }
