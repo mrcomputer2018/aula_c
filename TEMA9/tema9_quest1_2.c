0eeba531fe93ca05b9e977b84ce85c5881394fc2
Marcelo Ribeiro <mrcomputerinfo@gmail.com>
2020-05-12 14:00:53 -0300

    criando funcao de parametro

--- TEMA9/tema9_quest1_2.c
+++ TEMA9/tema9_quest1_2.c
@@ -8,6 +8,18 @@ main()
   printf("\nEntre com um numero inteiros nao-nulo:  ");
   scanf("%d", &numero);
   quadrado = numero * numero;
-  printf("O quadrado do numero %d e' %d\n", numero, quadrado);
+  printf("O quadrado do numero %d e %d\n", numero, quadrado);
   
 } */
+#include <stdio.h>
+
+int quadrado(int numero) {
+    numero = numero*numero;
+    return numero;
+}
+int main(){
+    int quadrado2, numero2;
+    printf("Digite o numero: \n"); scanf("%d", &numero2);
+    quadrado2 = quadrado(numero2);
+    printf("O quadrado do numero %d e' %d\n", numero2, quadrado2);
+}
