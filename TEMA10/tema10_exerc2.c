9979fee91784c4e487f3ebfcfdb4307d1adea3d3
Marcelo Ribeiro <mrcomputerinfo@gmail.com>
2020-05-13 11:28:21 -0300

    criando programe funcao condicional de diferenca

---
+++ TEMA10/tema10_exerc2.c
@@ -0,0 +1,19 @@
+// Crie uma função que receba um valor e diga se é nulo ou não.
+#include <stdio.h>
+
+char *nulo(int a) {
+    if (a!=0)
+    {
+        return " Numero nao e nulo";
+    }
+    else
+    {
+        return] "Numero e nulo";
+    }
+}
+
+int main() {
+    int numero;
+    printf("Digite um nunero: "); scanf("%d", &numero);
+    printf("%s\n", nulo(numero));
+}
