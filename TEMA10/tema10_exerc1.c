1be7eb58a9200abc42678a565a7df38ad56d3114
Marcelo Ribeiro <mrcomputerinfo@gmail.com>
2020-05-13 11:15:59 -0300

    criando programa e funcao condicional

---
+++ TEMA10/tema10_exerc1.c
@@ -0,0 +1,19 @@
+// Crie uma função que receba um valor e informe se ele é positivo ou não.
+#include <stdio.h>
+
+char *testaNumero (int a) {
+    if (a<0)
+    {
+        return "Numero e positivo"
+    }
+    else
+    {
+        return "Numero e negativo"
+    }
+}
+
+int main(){
+    int numero;
+    printf("Digite um numero: \n"); scanf("%d", &numero);
+    printf("%s\n", testaNumero(numero));
+}
