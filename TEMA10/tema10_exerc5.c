261ac27a560e42e0964409fec209b31dca2e32bc
Marcelo Ribeiro <mrcomputerinfo@gmail.com>
2020-05-14 09:31:33 -0300

    criacaoe acertos

--- TEMA10/tema10_exerc5.c
+++ TEMA10/tema10_exerc5.c
@@ -1,1 +1,20 @@
 // Crie uma função em linguagem C que receba 2 números e retorne o menor valor.
+#include <stdio.h>
+int menor(int a, int b){
+    if (a<=b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int main() {
+    int num1, num2;
+    
+    printf("Digite o 1o. numero: \n"); scanf("%d", &num1);
+    printf("Digite o 2o. numero: \n"); scanf("%d", &num2);
+    printf("Maior numero e : %d \n", menor(num1,num2)); 
+}
